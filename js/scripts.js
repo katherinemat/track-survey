42ceac195a176f4399a1f6495322893765c5891f
Katherine Matthews <katherinematthews.polsci@gmail.com>
2017-01-13 12:06:18 -0800

    add third question about website content whose results get stored into a variable and if user answered that they prefer building website content to applications then php drupal gets displayed.

--- js/scripts.js
+++ js/scripts.js
@@ -6,17 +6,20 @@ $(document).ready(function() {
 
     var userName = $("input#name").val();
 
-    var frontEndBackEndText = $("#frontEndBackEnd option:selected").text();
-    alert(frontEndBackEndText);
+    // var frontEndBackEndText = $("#frontEndBackEnd option:selected").text();
+    // alert(frontEndBackEndText);
 
     var frontEndBackEndVal = parseInt($("#frontEndBackEnd option:selected").val());
     alert(frontEndBackEndVal);
 
-    var typeCompanyText = $("#typeCompany option:selected").text();
-    alert(typeCompanyText);
+    // var typeCompanyText = $("#typeCompany option:selected").text();
+    // alert(typeCompanyText);
 
     var typeCompanyVal = parseInt($("#typeCompany option:selected").val());
     alert(typeCompanyVal);
+
+    var contentTypeVal = parseInt($("input:radio[name=contentType]:checked").val());
+    alert(contentTypeVal);
 
     $(".userName").append(userName);
     $("#surveyComplete").show();
@@ -25,6 +28,8 @@ $(document).ready(function() {
       $("#cssDesign").show();
     } else if (typeCompanyVal < 1) {
       $("#rubyRails").show();
+    } else if (contentTypeVal < 1) {
+      $("#phpDrupal").show();
     }
 
   });
