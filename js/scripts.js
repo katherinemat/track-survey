01d5d47077a0cef739642558d70f04322f0c9b34
Katherine Matthews <katherinematthews.polsci@gmail.com>
2017-01-13 11:16:19 -0800

    fix frontEndBackEnd variable by adding option selected to selected value.

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,10 @@
 $(document).ready(function() {
   $("form#learningPreferences").submit(function(event) {
     event.preventDefault();
-    //
+
     var userName = $("input#name").val();
-    // // var frontEndBackEnd = parseInt($("#frontEndBackEnd").val());
-    //
+    var frontEndBackEnd = parseInt($("#frontEndBackEnd option:selected").val());
+    alert(frontEndBackEnd);
     $(".userName").append(userName);
     $("#surveyComplete").show();
 
