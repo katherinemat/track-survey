0934c437a161de546055ce34857af5436eccc982
Katherine Matthews <katherinematthews.polsci@gmail.com>
2017-01-13 11:45:13 -0800

    add second question about kind of company and add else if statement to display ruby on rails if user wants startup company.

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,27 @@ $(document).ready(function() {
     event.preventDefault();
 
     var userName = $("input#name").val();
-    var frontEndBackEnd = $("#frontEndBackEnd option:selected").text();
-    alert(frontEndBackEnd);
 
+    var frontEndBackEndText = $("#frontEndBackEnd option:selected").text();
+    alert(frontEndBackEndText);
+
+    var frontEndBackEndVal = parseInt($("#frontEndBackEnd option:selected").val());
+    alert(frontEndBackEndVal);
+
+    var typeCompanyText = $("#typeCompany option:selected").text();
+    alert(typeCompanyText);
+
+    var typeCompanyVal = parseInt($("#typeCompany option:selected").val());
+    alert(typeCompanyVal);
 
     $(".userName").append(userName);
     $("#surveyComplete").show();
 
-    if (frontEndBackEnd === "Front-end design") {
+    if (frontEndBackEndVal < 1) {
       $("#cssDesign").show();
-    } 
+    } else if (typeCompanyVal < 1) {
+      $("#rubyRails").show();
+    }
 
   });
 });
